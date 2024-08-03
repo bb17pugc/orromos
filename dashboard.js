10a44c393fe5509948c83bc0a026ece3347053f2
muhammadsaad0077 <saaditanoli12@gmail.com>
2024-08-03 19:04:22 +0500

    dashboard page

---
+++ dashboard.js
@@ -0,0 +1,22 @@
+function toggleDropdown() {
+    document.getElementById('dropdown-menu').classList.toggle('show');
+}
+
+function setLanguage(language) {
+    document.querySelector('.dropdown button').innerHTML = `${language} <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+        <path fill-rule="evenodd" d="M5.293 7.707a1 1 0 011.414 0L10 11.414l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+    </svg>`;
+    toggleDropdown();
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown button')) {
+        var dropdowns = document.getElementsByClassName("dropdown-menu");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
